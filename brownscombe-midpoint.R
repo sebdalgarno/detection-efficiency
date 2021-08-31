f8b25dbb55c265277976ca40a177c4ae5d236464
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-08-31 16:44:02 -0700

    habitat model before reparametrising for random effects

--- brownscombe-midpoint.R
+++ brownscombe-midpoint.R
@@ -23,21 +23,108 @@ ggplot(detrange, aes(x = Distance, y = Detects/Pings, col = Habitat)) +
   xlab("Distance (m)") + 
   ylab("Detection efficiency")
 
-data <- detrange %>% filter(Station == "BTT17")
+data <- detrange 
+model_distance <- model("model {
 
-model_slope <- model("model {
-  bDistance ~ dnorm(0, 2^-2)
+  muint ~ dnorm(5, 2^-2)
+  sigmaint ~ dnorm(0, 5^-2) T(0,)
+
+  mubdist ~ dnorm(0, 0.1^-2)
+  sigmabdist ~ dnorm(0, 0.1^-2) T(0,)
+
+  for(i in 1:nStation) {
+    b0[i] ~ dnorm(muint, sigmaint^-2)
+    bDistance[i] ~ dnorm(mubdist, sigmabdist^-2)
+  }
+
   for(i in 1:nObs) {
-    logit(eDetects[i]) <- 10 + bDistance * Distance[i]
+    logit(eDetects[i]) <- b0[Station[i]] + bDistance[Station[i]] * Distance[i]
     Detects[i] ~ dbin(eDetects[i], Pings[i])
   }
 }", new_expr = "
- for (i in 1:nObs) {
-    logit(eDetects[i]) <- 10 + bDistance * Distance[i]
-    midpoint[i] <- -b0/bDistance
-   prediction[i] <- eDetects[i]
- }
-", nthin = 10L)
+  for(i in 1:nObs) {
+    logit(eDetects[i]) <- b0[Station[i]] + bDistance[Station[i]] * Distance[i]
+    prediction[i] <- eDetects[i]
+  }
+", nthin = 20L)
+
+analysis <- analyse(model_distance, data = data)
+print(coef(analysis, simplify = TRUE), n = 100)
+plot(analysis)
+
+prediction <- predict(analysis, new_data = c("Distance", "Station"))
+
+### next steps - try model with y-intercept at 100m
+# try model with habitat as fixed effect
+# compare model fit
+data$Detects %<>% as.integer()
+data$Pings %<>% as.integer()
+model_habitat <- model("model {
+
+  muint ~ dnorm(5, 2^-2)
+  sigmaint ~ dnorm(0, 5^-2) T(0,)
+
+  mubdist ~ dnorm(0, 0.1^-2)
+  sigmabdist ~ dnorm(0, 0.1^-2) T(0,)
+
+  for(i in 1:nStation) {
+    b0[i] ~ dnorm(muint, sigmaint^-2)
+    bDistance[i] ~ dnorm(mubdist, sigmabdist^-2)
+  }
+  
+  bHabitat[1] <- 0
+  for(i in 2:nHabitat) {
+    bHabitat[i] ~ dnorm(0, 10^-2)
+  }
+
+  for(i in 1:nObs) {
+    logit(eDetects[i]) <- b0[Station[i]] + bDistance[Station[i]] * Distance[i] + bHabitat[Habitat[i]] 
+    Detects[i] ~ dbin(eDetects[i], Pings[i])
+  }
+}", new_expr = "
+  for(i in 1:nObs) {
+    logit(eDetects[i]) <- b0[Station[i]] + bDistance[Station[i]] * Distance[i] + bHabitat[Habitat[i]] 
+    prediction[i] <- eDetects[i]
+  }
+", nthin = 50L, select_data = list(Detects = c(0L, 10000L), 
+                                   `Distance+` = c(0, 1000),
+                                   Habitat = factor(),
+                                   Station = factor(),
+                                   Pings = c(1L, 10000L)), random_effects = list(bDistance = "Station",
+                                                                                 b0 = "Station"))
+
+analysis_habitat <- analyse(model_habitat, data = data)
+print(coef(analysis_habitat, simplify = TRUE), n = 100)
+plot(analysis_habitat)
+
+habitat <- data %>% select(Station, Habitat)
+prediction_habitat <- predict(analysis_habitat, new_data = c("Distance", "Station", "Habitat")) %>%
+  left_join(habitat, "Station") %>%
+  filter(Habitat.x == Habitat.y) %>%
+  rename(Habitat = Habitat.x)
+
+# how do I sample from the posterior to generate a distribution from each distance point not just estimate and upper/lower?
+# i.e. as in mcelreath link function where each column is a distance value and each row is a sample of mu
+# can we recreate plot similar to figure 4.10 in mcelreath? - parameter CI vs simulated outcome (predicted interval) CI - or same in this case since there is no sigma param in dbinom
+ggplot(data = prediction_habitat, aes(x = Distance, y = estimate, colour = Habitat)) +
+  # geom_point(data = data, aes(y = Detects/Pings)) +
+  geom_line() +
+  geom_line(aes(y = lower), linetype = "dotted") +
+  geom_line(aes(y = upper), linetype = "dotted") +
+  facet_wrap(~Station) +
+  theme(legend.position = "none") +
+  scale_y_continuous(
+    trans = scales::logit_trans(),
+  ) +
+  NULL
+
+ggplot(data = prediction, aes(x = Distance + 100, y = estimate)) +
+  geom_point(data = data, aes(y = Detects/Pings)) +
+  geom_line() +
+  geom_line(aes(y = lower), linetype = "dotted") +
+  geom_line(aes(y = upper), linetype = "dotted") +
+  facet_wrap(~Station) +
+  NULL
 
 model_midpoint <- model("model {
   bMidpoint ~ dunif(0, 1000)
@@ -52,20 +139,8 @@ model_midpoint <- model("model {
  }
 ", nthin = 10L)
 
-analysis <- analyse(model_slope, data = data)
-coef(analysis, simplify = TRUE)
-plot(analysis)
-
 prediction_midpoint <- predict(analysis, new_data = character(0), 
                                term = "midpoint")
-prediction <- predict(jags_analysis, new_data = "Distance")
-
-ggplot(data = prediction, aes(x = Distance, y = estimate)) +
-  geom_point(data = data, aes(y = Detects/Pings)) +
-  geom_line() +
-  geom_line(aes(y = lower), linetype = "dotted") +
-  geom_line(aes(y = upper), linetype = "dotted") +
-  NULL
 
 
 
